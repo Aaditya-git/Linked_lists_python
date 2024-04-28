180083a1ce7f7dc17a27525ec9f9b54676e37bed
Aaditya Bhilegaonkar <aadityab134@gmail.com>
2024-04-28 14:40:11 +0530

    Linked List basic operations

---
+++ main.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def printlist(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result = result + '->'
+            temp_node = temp_node.next
+
+        return result
+
+
+
+new_ll = LinkedList()
+new_ll.append(10)
+new_ll.append(20)
+new_ll.prepend(30)
+result = new_ll.printlist()
+print(result)
